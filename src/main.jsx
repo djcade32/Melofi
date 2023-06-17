b4c2f987638e9974d70382088824b565642f6824
Norman Cade <norman_cade32@yahoo.com>
2023-06-17 19:06:58 -0400

    google analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAWo5QaovbZdxm1iPhX7RTpgSC7iExTR3w",
+  authDomain: "melofi-389415.firebaseapp.com",
+  projectId: "melofi-389415",
+  storageBucket: "melofi-389415.appspot.com",
+  messagingSenderId: "404248652005",
+  appId: "1:404248652005:web:926c9820ee3780aa7d4c0c",
+  measurementId: "G-79SS2N9YZQ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
