c7321cd1c26fda89563cbcfbfb60c3b4a688fcbe
Norman Cade <norman_cade32@yahoo.com>
2023-07-30 09:09:42 -0400

    Fixed screen darkening bug on component opens

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 // import AppHome from "./pages/AppHome";
+// import PrivacyPolicy from "./pages/PrivacyPolicy"
 const AppHome = React.lazy(() => import("./pages/AppHome"));
 const PrivacyPolicy = React.lazy(() => import("./pages/PrivacyPolicy"));
 
